2a7d6e2e40af902c32b3c7dd393c38b1d671fe0c
Mark Westbrook <westbrma@gmail.com>
2026-09-24 17:45:16 -0500

    update

--- examples/example.js
+++ examples/example.js
@@ -1,14 +1,17 @@
 async function load_file(file, title, lang = 'html') {
+	let div = document.createElement('div');
+	document.body.appendChild(div);
+
 	let code = await fetch(file).then((r) => r.text());
 	if (lang === 'html') {
 		code = code.replaceAll('<', '&lt;');
 		code = code.replaceAll('>', '&gt;');
 	}
-	let div = document.createElement('div');
+
 	div.innerHTML = `<h3>${title}</h3>` + `<pre><code class="language-${lang}">${code}</code></pre>`;
-	document.body.appendChild(div);
+
 	hljs.highlightAll();
 }
 
-load_file('/examples/button.html', 'For Loop!', 'html');
-load_file('/examples/for_loop.js', 'For Loop!', 'javascript');
+load_file('button.html', 'For Loop!', 'html');
+load_file('for_loop.js', 'For Loop!', 'javascript');
